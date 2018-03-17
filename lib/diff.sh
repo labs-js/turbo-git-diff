07db902056b072713c6df09d3a2eae609e939ec0
Samuel Fabrizio <samuelfabrizio@gmail.com>
2018-03-17 11:42:39 +0100

    [FIX] diff-so-fancy path alternative for global moduleundefined

--- lib/diff.sh
+++ lib/diff.sh
@@ -2,7 +2,13 @@
 # I took part of this script from here:
 # https://github.com/zdharma/zsh-diff-so-fancy/blob/master/bin/git-dsf
 
-path="$(dirname $0)/../node_modules/diff-so-fancy/diff-so-fancy"
+path="$(dirname $0)/../../node_modules/diff-so-fancy/diff-so-fancy"
+
+if [ ! -f $path ]; then
+    path="$(dirname $0)/../node_modules/diff-so-fancy/diff-so-fancy"
+else
+    echo "error: diff-so-fancy path not found"
+fi
 
 f() {
     [ -z "$GIT_PREFIX" ] || \
