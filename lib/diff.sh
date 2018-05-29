ab3617edc1bf743f5bff5f387c5db469bbc07e9e
Samuel Fabrizio <samuelfabrizio@gmail.com>
2018-05-29 09:48:12 +0200

    [ADD] fallback to normal diff on windows

--- lib/diff.sh
+++ lib/diff.sh
@@ -1,13 +1,23 @@
-#!/bin/sh
+#!/usr/bin/sh
 # I took part of this script from here:
 # https://github.com/zdharma/zsh-diff-so-fancy/blob/master/bin/git-dsf
+
+os_name=""
+
+if [[ $(uname -s) == Darw* ]]; then
+    os_name="osx"
+elif [[ "$(expr substr $(uname -s) 1 10)" == MINGW* ]]; then
+    os_name="windows"
+    git diff --color "$@"
+    exit 0;
+fi
 
 path="$(dirname $0)/../../node_modules/diff-so-fancy/diff-so-fancy"
 
 if [ ! -f $path ]; then
     path="$(dirname $0)/../node_modules/diff-so-fancy/diff-so-fancy"
 else
-    echo "error: diff-so-fancy path not found"
+    echo "turbo-git-diff error: diff-so-fancy path not found"
 fi
 
 f() {
@@ -16,4 +26,4 @@ f() {
         git diff --color "$@" | $path | less --tabs=4 -iRFX
     }
 
-f "$@"
+f "$@"usr
